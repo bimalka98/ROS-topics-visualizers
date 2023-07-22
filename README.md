60352810b48750eb92e4a48d0f24c48a02f77d45
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2023-07-22 21:12:38 +0530

    add code for final results

--- README.md
+++ README.md
@@ -25,8 +25,181 @@ twist_linear_x: min = 0.00       max = 0.05       avg = 0.05
 twist_angular_z:min = 0.00       max = 0.05       avg = 0.05
 ```
 
-## Figures
+# Odometry messages data
 
-![](figures/LiDAR/odom_pose_viz_200s.png)
-![](figures/LiDAR/odom_twist_viz_200s.png)
+```shell
+bimalka98@LAP-BIMALKA98:~$ ./odom.sh 
+/odom/lidar
+Type: nav_msgs/Odometry
 
+Publishers: 
+ * /play_1690038154324027230 (http://LAP-BIMALKA98:43943/)
+
+Subscribers: None
+
+
+header: 
+  seq: 16
+  stamp: 
+    secs: 1689233055
+    nsecs:  37512406
+  frame_id: "odom"
+child_frame_id: "base_link"
+pose: 
+  pose: 
+    position: 
+      x: 0.23845223652010977
+      y: 0.6142565424490137
+      z: -0.011431738076478983
+    orientation: 
+      x: 0.0033254036126051656
+      y: 0.004497113294042495
+      z: 0.036553529440019425
+      w: 0.9993160446767136
+  covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+twist: 
+  twist: 
+    linear: 
+      x: 0.10810701618394586
+      y: -0.056123804611706796
+      z: 0.0004582837412625423
+    angular: 
+      x: 0.00017835980908364397
+      y: -1.733515524174483e-05
+      z: -0.0021564923982788195
+  covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+---
+/odom/wheel
+Type: nav_msgs/Odometry
+
+Publishers: 
+ * /play_1690038154324027230 (http://LAP-BIMALKA98:43943/)
+
+Subscribers: None
+
+
+header: 
+  seq: 117
+  stamp: 
+    secs: 1689233056
+    nsecs: 804661339
+  frame_id: "odom"
+child_frame_id: "base_link"
+pose: 
+  pose: 
+    position: 
+      x: 0.025897095203399653
+      y: 0.0
+      z: 0.0
+    orientation: 
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 0.0
+  covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+twist: 
+  twist: 
+    linear: 
+      x: 0.0
+      y: 0.0
+      z: 0.0
+    angular: 
+      x: 0.0
+      y: 0.0
+      z: 0.0
+  covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+---
+/imu/data
+Type: sensor_msgs/Imu
+
+Publishers: 
+ * /play_1690038154324027230 (http://LAP-BIMALKA98:43943/)
+
+Subscribers: None
+
+
+header: 
+  seq: 12
+  stamp: 
+    secs: 0
+    nsecs:         0
+  frame_id: ''
+orientation: 
+  x: 359.8125
+  y: -5.25
+  z: 1.3125
+  w: 0.0
+orientation_covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+angular_velocity: 
+  x: -0.0021816615480929613
+  y: -0.0021816615480929613
+  z: 0.003272492438554764
+angular_velocity_covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+linear_acceleration: 
+  x: 0.029999999329447746
+  y: 0.009999999776482582
+  z: -0.20999999344348907
+linear_acceleration_covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+---
+/robot_rtk_xyz
+Type: geometry_msgs/PointStamped
+
+Publishers: 
+ * /play_1690038154324027230 (http://LAP-BIMALKA98:43943/)
+
+Subscribers: None
+
+
+header: 
+  seq: 7
+  stamp: 
+    secs: 1689233060
+    nsecs: 922173500
+  frame_id: "map"
+point: 
+  x: -3.347173924976582
+  y: 3.020952906490796
+  z: 0.0
+---
+/odometry/filtered
+Type: nav_msgs/Odometry
+
+Publishers: 
+ * /play_1690038154324027230 (http://LAP-BIMALKA98:43943/)
+
+Subscribers: None
+
+
+header: 
+  seq: 94
+  stamp: 
+    secs: 1689233062
+    nsecs: 637989759
+  frame_id: "odom"
+child_frame_id: "base_link"
+pose: 
+  pose: 
+    position: 
+      x: 0.025910185575430438
+      y: 0.0
+      z: 0.0
+    orientation: 
+      x: 0.0
+      y: 0.0
+      z: 0.0
+      w: 1.0
+  covariance: [9.99999400027635e-10, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 9.999994000276347e-10, 0.0, 0.0, 0.0, -3.1495764014662536e-14, 0.0, 0.0, 4.99875067735381e-07, 0.0, 5.816467904292292e-21, 0.0, 0.0, 0.0, 0.0, 4.997502602951425e-07, 0.0, 0.0, 0.0, 0.0, 5.8164679042922926e-21, 0.0, 4.997502602951425e-07, 0.0, 0.0, -3.149576401466254e-14, 0.0, 0.0, 0.0, 0.5630291723790509]
+twist: 
+  twist: 
+    linear: 
+      x: -6.573883017878992e-12
+      y: 0.0
+      z: 0.0
+    angular: 
+      x: 0.0
+      y: 0.0
+      z: 0.0
+  covariance: [9.999988250845988e-10, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 9.999988250845988e-10, 0.0, 0.0, 0.0, 1.4591400975545615e-39, 0.0, 0.0, 4.998126484268216e-07, 0.0, 3.6115345217972575e-30, 0.0, 0.0, 0.0, 0.0, 4.992522728785569e-07, 0.0, 0.0, 0.0, 0.0, 3.611534521797257e-30, 0.0, 4.992522728785569e-07, 0.0, 0.0, 1.4591400975545615e-39, 0.0, 0.0, 0.0, 9.999985000674396e-10]
+---
+bimalka98@LAP-BIMALKA98:~$
+```
--- scripts/odom_fusion_logger.py
+++ scripts/odom_fusion_logger.py
@@ -4,6 +4,8 @@ import os
 import rospkg
 import rospy
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import PointStamped
 from geometry_msgs.msg import Twist
 
 from tf.transformations import euler_from_quaternion
@@ -19,26 +21,48 @@ if os.path.exists(csv_path):
 
 # Initialize variables to store data
 
-# pose and orientation
-odom_x_pose_lidar = 0
-odom_y_pose_lidar = 0
-odom_x_pose_filtered = 0
-odom_y_pose_filtered = 0
+# lidar odometry: 
+# /odom/lidar
+# Type: nav_msgs/Odometry
+odom_x_pose_lidar = 0; odom_y_pose_lidar = 0; odom_lin_x_lidar = 0; odom_ang_z_lidar = 0
 
-# velocities
-odom_lin_x_lidar = 0
-odom_ang_z_lidar = 0
-odom_lin_x_filtered = 0
-odom_ang_z_filtered = 0
+# wheel odometry: 
+# topic: /odom/wheel
+# Type: nav_msgs/Odometry
+odom_x_pose_wheel = 0; odom_y_pose_wheel = 0; odom_lin_x_wheel = 0; odom_ang_z_wheel = 0
+
+# imu odometry
+# topic: /imu/data
+# Type: sensor_msgs/Imu
+odom_x_pose_imu = 0; odom_y_pose_imu = 0; odom_lin_x_imu = 0; odom_ang_z_imu = 0
+
+# gps odometry
+# topic: /robot_rtk_xyz
+# Type: geometry_msgs/PointStamped
+odom_x_pose_gps = 0; odom_y_pose_gps = 0; odom_lin_x_gps = 0; odom_ang_z_gps = 0
+
+# fused odometry
+# topic: /odometry/filtered
+# Type: nav_msgs/Odometry
+odom_x_pose_filtered = 0; odom_y_pose_filtered = 0; odom_lin_x_filtered = 0; odom_ang_z_filtered = 0
+
+def write_to_file():
+    # Get current time in seconds
+    t = rospy.get_time()
+
+    # Write data to CSV file
+    with open(csv_path, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([t, 
+                            odom_x_pose_lidar, odom_y_pose_lidar, odom_lin_x_lidar, odom_ang_z_lidar, 
+                            odom_x_pose_wheel, odom_y_pose_wheel, odom_lin_x_wheel, odom_ang_z_wheel,
+                            odom_x_pose_imu, odom_y_pose_imu, odom_lin_x_imu, odom_ang_z_imu,
+                            odom_x_pose_gps, odom_y_pose_gps, odom_lin_x_gps, odom_ang_z_gps,
+                            odom_x_pose_filtered, odom_y_pose_filtered, odom_lin_x_filtered, odom_ang_z_filtered])
 
 
 def lidar_odometry_callback(data):
-
     global odom_x_pose_lidar, odom_y_pose_lidar, odom_lin_x_lidar, odom_ang_z_lidar
-    global odom_x_pose_filtered, odom_y_pose_filtered, odom_lin_x_filtered, odom_ang_z_filtered
-
-    # Get current time in seconds
-    t = rospy.get_time()
 
     # Get pose
     odom_x_pose_lidar = data.pose.pose.position.x
@@ -48,20 +72,27 @@ def lidar_odometry_callback(data):
     odom_lin_x_lidar = data.twist.twist.linear.x
     odom_ang_z_lidar = data.twist.twist.angular.z
 
+    # Write data to CSV file
+    write_to_file()
+    
+def wheel_odometry_callback(data):        
+    global odom_x_pose_wheel, odom_y_pose_wheel, odom_lin_x_wheel, odom_ang_z_wheel
+    
+    # Get pose
+    odom_x_pose_wheel = data.pose.pose.position.x
+    odom_y_pose_wheel = data.pose.pose.position.y
+
+    # Get linear and angular velocities
+    odom_lin_x_wheel = data.twist.twist.linear.x
+    odom_ang_z_wheel = data.twist.twist.angular.z
 
     # Write data to CSV file
-    with open(csv_path, 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow([t, odom_x_pose_lidar, odom_y_pose_lidar, odom_lin_x_lidar, odom_ang_z_lidar, odom_x_pose_filtered, odom_y_pose_filtered, odom_lin_x_filtered, odom_ang_z_filtered])
+    write_to_file()
+
 
 def fused_odometry_callback(data):
-    
-    global odom_x_pose_lidar, odom_y_pose_lidar, odom_lin_x_lidar, odom_ang_z_lidar
     global odom_x_pose_filtered, odom_y_pose_filtered, odom_lin_x_filtered, odom_ang_z_filtered
 
-    # Get current time in seconds
-    t = rospy.get_time()
-    
     # Get pose
     odom_x_pose_filtered = data.pose.pose.position.x
     odom_y_pose_filtered = data.pose.pose.position.y
@@ -71,11 +102,51 @@ def fused_odometry_callback(data):
     odom_ang_z_filtered = data.twist.twist.angular.z
 
     # Write data to CSV file
-    with open(csv_path, 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow([t, odom_x_pose_lidar, odom_y_pose_lidar, odom_lin_x_lidar, odom_ang_z_lidar, odom_x_pose_filtered, odom_y_pose_filtered, odom_lin_x_filtered, odom_ang_z_filtered])
+    write_to_file()
+
+def imu_callback(data):
+    global odom_x_pose_imu, odom_y_pose_imu, odom_lin_x_imu, odom_ang_z_imu
+
+    # Get pose
+    # Note: IMU data typically does not provide position information
+    # You may need to integrate the linear acceleration data to get position
+    # Or use some other method to estimate position from the IMU data
+    odom_x_pose_imu = 0
+    odom_y_pose_imu = 0
+
+    # Get linear and angular velocities
+    # Note: The IMU message provides angular velocity and linear acceleration data
+    # You may need to convert the linear acceleration to linear velocity
+    # Or use some other method to estimate linear velocity from the IMU data
+    odom_lin_x_imu = 0
+    odom_ang_z_imu = data.angular_velocity.z
+
+    # Write data to CSV file
+    write_to_file()
+
+def gps_callback(data):
+    global odom_x_pose_gps, odom_y_pose_gps, odom_lin_x_gps, odom_ang_z_gps
+
+    # Get pose
+    odom_x_pose_gps = data.point.x
+    odom_y_pose_gps = data.point.y
+
+    # Get linear and angular velocities
+    # Note: GPS data typically does not provide velocity information
+    # You may need to differentiate the position data to get velocity
+    # Or use some other method to estimate velocity from the GPS data
+    odom_lin_x_gps = 0
+    odom_ang_z_gps = 0
+
+    # Write data to CSV file
+    write_to_file()
+
 
 rospy.init_node('data_logger')
-rospy.Subscriber('/odometry/filtered', Odometry, fused_odometry_callback)
+# Subscribe to topics
 rospy.Subscriber('/odom/lidar', Odometry, lidar_odometry_callback)
+rospy.Subscriber('/odom/wheel', Odometry, wheel_odometry_callback)
+rospy.Subscriber('/imu/data', Imu, imu_callback)
+rospy.Subscriber('/robot_rtk_xyz', PointStamped, gps_callback)
+rospy.Subscriber('/odometry/filtered', Odometry, fused_odometry_callback)
 rospy.spin()
